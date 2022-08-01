77e7bf0306cc3f4257b51fd11c04c1e447ff8ad3
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-08-01 15:14:49 -0400

    primer commit
    proyecto todojunto para clases de react.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {NuevoComponente} from './Componentes/NuevoComponente'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p children={'Este es un hijo'}></p>
+      <h1>Poner Todo Junto</h1>
+      <NuevoComponente header={"Header Props"}></NuevoComponente>
     </div>
   );
 }
---
+++ src/Componentes/NuevoComponente.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export class NuevoComponente extends Component{
+
+    render(){
+        return(
+            <div>                
+                <h1 style={{color:'red'}}>
+                    {this.props.header}
+                </h1>
+                {this.props.children}                
+            </div>
+        );
+    }
+
+}
