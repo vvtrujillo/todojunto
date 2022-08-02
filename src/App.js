4a41a0b957a1cc889b3c2f405a168ab700dc730d
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-08-02 17:34:42 -0400

    semaforo
    agrega funciones para cambiar colores a las luces del semaforo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import {NuevoComponente} from './Componentes/NuevoComponente'
+import { useState } from 'react';
+import { render } from '@testing-library/react';
 
 const App = () => {
-  return (
+
+  const[interruptor, setInterruptor] = useState({
+    position: 'on',
+    colorSem: 'green'
+  });
+
+  const presionaBoton = () => {
+    alert('Presione el botón!!!');
+  }
+
+
+  const cambiaBoton = () => {
+    
+    console.log('ingreso position: ' + interruptor.position + ' colorSem: ' + interruptor.colorSem);
+    
+    if(interruptor.position=='on'){
+
+      interruptor.position = 'off';
+      interruptor.colorSem='red';
+      
+    }
+    else{
+
+      interruptor.position='on';
+      interruptor.colorSem='green';
+     
+    }
+    console.log('salida position: ' + interruptor.position + ' colorSem: ' + interruptor.colorSem);
+  }
+
+  return (    
     <div className="App">
-      <p children={'Este es un hijo'}></p>
+      {/*<p children={'Este es un hijo'}></p>
       <h1>Poner Todo Junto</h1>
-      <NuevoComponente header={"Header Props"}></NuevoComponente>
+      <button onClick={presionaBoton}>Presioname</button>*/}
+      <button onClick={cambiaBoton}>apagar</button>
+      <div className='semaforo'>
+        <div className='circulo-semaforo' style={{backgroundColor: interruptor.colorSem}}></div>
+        <div className='circulo-semaforo' style={{backgroundColor: interruptor.colorSem}}></div>
+        <div className='circulo-semaforo' style={{backgroundColor: interruptor.colorSem}}></div>
+      </div>       
+      {/*<NuevoComponente header={"Header Props"}></NuevoComponente>*/}
     </div>
   );
 }
--- src/Componentes/NuevoComponente.jsx
+++ src/Componentes/NuevoComponente.jsx
@@ -12,5 +12,4 @@ export class NuevoComponente extends Component{
             </div>
         );
     }
-
 }
